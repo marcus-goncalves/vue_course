6874525f7fb2f9bf8a10db4e312e7fcb026eb0a7
Marcus Gonçalves <nvrrox@gmail.com>
2020-10-28 23:56:31 -0300

    updating classes

--- 2_components/app.js
+++ 2_components/app.js
@@ -1,3 +1,4 @@
+
 Vue.component('click-counter', {
     template: '<button @click="count++">{{count}}</button>',
     data() {
@@ -16,25 +17,57 @@ Vue.component('click-counter2', {
     }
 })
 
-Vue.component('plan-picker', {
-    template: '#plan-picker-template',
-    data() {
-        return {
-            plans: ['The Hacker', 'The Single', 'The Curious', 'The Addict']
-        }
-    }
-})
-
-Vue.component('plan', {
+let PlanComponent = {
     template: '#plan-template',
     props: {
         name: {
             type: String,
             required: true
+        },
+        selectedPlan: {
+            type: String
+        }
+    },
+    data() {
+        return {
+            selected: false
+        }
+    },
+    methods: {
+        select() {
+            this.$emit('select', this.name)
+        }
+    },
+    computed: {
+        isSelected() {
+            return this.name  === this.selectPlan
         }
     }
-})
+}
+
+let PlanPickerComponent = {
+    template: '#plan-picker-template',
+    components: {
+        plan: PlanComponent
+    },
+    data() {
+        return {
+            plans: ['The Hacker', 'The Single', 'The Curious', 'The Addict'],
+            selectedPlan: null
+        }
+    },
+    methods: {
+        selectPlan(plan) {
+            this.selectedPlan = plan
+        }
+    }
+ 
+
+}
 
 new Vue({
-    el: '#app'
+    el: '#app',
+    components: {
+        PlanPicker: PlanPickerComponent
+    }
 })
