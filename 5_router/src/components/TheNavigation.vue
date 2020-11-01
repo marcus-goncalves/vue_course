93d09998fb9fb6edbaa6705774f0452919f90079
Marcus Gonçalves <nvrrox@gmail.com>
2020-11-01 17:57:39 -0300

    router partial

---
+++ 5_router/src/components/TheNavigation.vue
@@ -0,0 +1,72 @@
+<template>
+  <nav id="nav">
+    <p class="logo">Vue School - Router Class</p>
+    <ul class="nav-links">
+      <li class="links">
+        <router-link to="/">
+          Home
+        </router-link>
+      </li>
+      <li 
+        v-for="destination in destinations"
+        :key="destination.name"
+        class="links"
+      >
+        <router-link :to="{name: 'DestinationDetails', params: {id: destination.id}}">
+          {{destination.name}}
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import store from '@/store.js';
+
+export default {
+  data() {
+    return {
+      destinationId: this.$route.params.id,
+      destinations: store.destinations
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #nav {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+    text-decoration: none;
+  }
+
+  #nav a.vue-link-active {
+    color: #42b983;
+  }
+
+  .nav-links {
+    display: flex;
+  }
+
+  .links {
+    padding-right: 20px;
+    list-style: none;
+  }
+
+  .links:hover {
+    text-decoration: underline;
+  }
+
+  .logo {
+    font-size: 20px;
+    color: purple;
+    font-weight: bold;
+  }
+
+</style>
---
+++ 5_router/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+import Home from "../views/Home.vue";
+
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: '/details/:id',
+    name: 'DestinationDetails',
+    component: () => import(/* webpackChunkName: "DestinationDetails" */ '../views/DestinationDetails.vue')
+  }
+];
+
+const router = new VueRouter({
+  linkExactActiveClass: 'vue-link-active',
+  routes
+});
+
+export default router;
