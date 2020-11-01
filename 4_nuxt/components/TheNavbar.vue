4e1f60e55b3758475c2018c30078672a2544298e
Marcus Gonçalves <nvrrox@gmail.com>
2020-11-01 15:19:57 -0300

    nuxt course

---
+++ 4_nuxt/components/TheNavbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="nav">
+    <div class="logo">
+      <nuxt-link class="logo text-lg" :to="{name: 'index'}">
+        Nuxt Fundamentals
+      </nuxt-link>
+
+      <span class="subheader">A Vue School Course</span>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ 4_nuxt/pages/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        4_nuxt
+      </h1>
+      <div class="links">
+        <nuxt-link 
+          v-for="post in posts" 
+          :key="post.id"
+          :to="{name: 'posts-id', params: {id: post.id}}"
+          class="button--grey"
+        >
+          {{post.title}}
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Logo from '@/components/Logo.vue';
+
+  export default {
+    components: {
+      Logo
+    },
+    computed: {
+      posts() {
+        return this.$store.state.posts.all
+      }
+    },
+    head() {
+      return {
+        title: 'Home Page'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  /* color: #35495e; */
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  /* padding-bottom: 15px; */
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
